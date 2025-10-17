c2a80cdb2a9d47e9f04d195cfcbd8bca818578c6
Burak Altundas <burakaltundas52@gmail.com>
2025-10-17 14:47:42 +0300

    .sh düzeltildi

--- build.sh
+++ build.sh
@@ -2,9 +2,21 @@
 # Herhangi bir komut başarısız olursa scripti durdur
 set -e
 
-# --- Flutter Kurulumu ---
-# Flutter'ı GitHub'dan klonla (belirttiğimiz versiyonu)
-git clone https://github.com/flutter/flutter.git --depth 1 --branch $FLUTTER_VERSION flutter
+# --- Flutter Kurulumu (Önbellek Dostu Versiyon) ---
+# 'flutter' adında bir klasör var mı diye kontrol et
+if [ -d "flutter" ] 
+then
+  # Eğer klasör varsa, içine gir ve sadece güncelle (bu daha hızlıdır)
+  echo "Mevcut Flutter SDK'sı güncelleniyor..."
+  cd flutter
+  git pull
+  cd ..
+else
+  # Eğer klasör yoksa (örneğin ilk build), Flutter SDK'sını klonla
+  echo "Flutter SDK klonlanıyor..."
+  git clone https://github.com/flutter/flutter.git --depth 1 --branch $FLUTTER_VERSION flutter
+fi
+
 # Flutter komutunu sistem yoluna ekle
 export PATH="$PATH:`pwd`/flutter/bin"
 
